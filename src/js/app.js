262baaab8433b95fd144b279b40e7306fc39ed7e
CuityMiko <15201776956@163.com>
2017-05-30 00:59:26 +0800

    完成webpack基本骨架

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,15 @@
-(function(document){
-    document.getElementById('app').innerHTML='webpack的快速入门test'
-    console.log('hello webpack')
-})(document)
+// 项目入口文件
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+
+import Hello from '../components/Hello.js'
+
+// console.log('我就用console')
+// console.log('我就用console')
+
+ReactDOM.render(
+    <div>
+       <Hello/>
+    </div>,
+    document.getElementById('app')
+)
--- webpack.develop.config.js
+++ webpack.develop.config.js
@@ -1,22 +1,27 @@
 /**
- * 项目开发配置文件
+ * 项目开发环境配置文件
  */
-var path=require('path');
+var path = require('path');
+// 自动打开浏览器插件
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
-module.exports={
-    entry:[ // entry：SPA入口文件
-        //浏览器自动刷新
+module.exports = {
+    // entry:path.resolve(__dirname,'src/js/app.js'),
+    entry:[
         'webpack/hot/dev-server',
-        'webpack-dev-server/client?http://localhost:8080', 
+        'webpack-dev-server/client?http://localhost:8080',
         path.resolve(__dirname,'src/js/app.js')
-    ], 
-    output:{
-        path:path.resolve(__dirname,'dist'), //webpack输出文件
-        filename:'bundle.js'
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
-    module: { //加载器
+    eslint: {
+        configFile: '.eslintrc.js'
+    },
+    module: {
         loaders: [
-            //处理jsx语法和es6语法
+            // 处理jsx语法和ES6语法
             {
                 test: /\.jsx?$/, // 用正则来匹配文件路径，这段意思是匹配 js 或者 jsx
                 loader: 'babel',// 加载模块 "babel" 是 "babel-loader" 的缩写
@@ -24,26 +29,48 @@ module.exports={
                     presets: ['es2015', 'react']
                 }
             },
-            //处理在js中引用css
+            {
+                test: /\.js$/,
+                loader: "eslint-loader",
+                exclude: /node_modules/
+            },
+            // 处理在js中引用css
             {
                 test: /\.css$/, // Only .css files
-                loader: 'style!css' // 如果有多个加载器，中间用!隔开，多个加载器的执行顺序是从右往左去执行
+                loader: 'style!css' // 如果有多个加载器，中间作用感叹号隔开，多个加载器是从右往左去执行
             },
-            //处理在js引用sess
             {
                 test: /\.scss$/,
                 loader: 'style!css!sass'
             },
-            //处理在js中引用图片
+            // 1kb=1024b 1b=8bit   25000bit~3kb
             {
-                test: /\.(png|jpg)$/,
+                test: /\.(png|jpg|jpeg|gif)$/,
                 loader: 'url?limit=25000'
             },
-            //处理字体文件
             {
-                test: /\.woff$/,
-                loader: 'url?limit=100000'
+                test: /\.(eot|woff|ttf|woff2|svg)$/,
+                loader: 'url'
             }
+
         ]
-    }
+    },
+    resolve: {
+        // 自动扩展文件后缀名，意味着我们require模块可以省略不写后缀名
+        // 注意一下, extensions 第一个是空字符串! 对应不需要后缀的情况.
+        extensions: ['', '.js', '.json', '.scss', '.jsx']
+        // 模块别名定义，方便后续直接引用别名，无须多写长长的地址。后续直接 require('AppStore') 即可
+        //alias: {
+        //    AppStore: 'js/stores/AppStores.js',
+        //    ActionType: 'js/actions/ActionType.js',
+        //    AppAction: 'js/actions/AppAction.js'
+        //}
+    },
+    plugins: [
+        new OpenBrowserPlugin({url: 'http://localhost:8080/', browser: 'chrome'})
+    ]
+
 }
+
+
+
